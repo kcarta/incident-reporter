5af09d6aeb8b07d8f9f447351749cc60aa0cac2b
Kristofer Carta <kriscarta@gmail.com>
2019-05-05 20:48:08 +0200

    Simplify incident model

--- IncidentReporter.Shared/Incident.cs
+++ IncidentReporter.Shared/Incident.cs
@@ -6,16 +6,16 @@ namespace IncidentReporter.Shared
     public class Incident
     {
         public string Id { get; set; }
-        public Boolean IsResolved { get; set; }
+        public bool IsResolved { get; set; }
         public DateTime Date { get; set; }
         public string Summary { get; set; }
         public string Impact { get; set; }
-        public List<string> RootCauses { get; set; }
+        public string RootCause { get; set; }
         public string Trigger { get; set; }
         public string Resolution { get; set; }
         public string Detection { get; set; }
+        public string LessonsLearned { get; set; }
         public List<ActionTaken> ActionsTaken { get; set; }
         public List<string> WorkItems { get; set; }
-        public List<string> LessonsLearned { get; set; }
     }
 }
